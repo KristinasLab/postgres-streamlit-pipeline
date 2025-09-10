e1d50c0eeb3e68632bd832b0cd28a8028ff5e0a5
KristinasLab <kris.lit3000@gmail.com>
2025-09-10 16:28:19 +0200

    database.py adjustments

--- docker/api/app/database.py
+++ docker/api/app/database.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
 # Load .env only for local development
 if os.getenv("RENDER") is None:
@@ -12,14 +13,14 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Fallback for local Docker setup
 if DATABASE_URL is None:
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_USER = quote_plus(os.getenv("DB_USER"))
+    DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
     DB_NAME = os.getenv("DB_NAME")
     DB_HOST = os.getenv("DB_HOST", "localhost")
     DB_PORT = os.getenv("DB_PORT", "5432")
     DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-# Create engine and session
+# SQLAlchemy setup
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
